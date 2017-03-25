4101b330a6b31e6140477241d8d1133ffa7424a2
ruslan-polutsygan <ruslan.polutsygan@gmail.com>
2017-03-25 23:23:36 +0200

    - check if other application installed - other-component

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,17 +2,23 @@ import {LoginComponent} from './pages/login/login.component';
 import {ListComponent} from "./pages/list/list.component";
 import {CameraComponent} from "./pages/camera/camera.component";
 import {LocationComponent} from "./pages/location/location.component";
+import {CheckAppComponent} from "./pages/check-app/check-app.component";
+import {OtherComponent} from "./pages/other/other.component";
 
 export const routes = [
     { path: '', component: LoginComponent },
     { path: 'list', component: ListComponent },
+    { path: 'other', component: OtherComponent },
     { path: 'camera', component: CameraComponent },
     { path: 'location', component: LocationComponent },
+    { path: 'check-app', component: CheckAppComponent },
 ];
 
 export const navigatableComponents = [
     LoginComponent,
     ListComponent,
+    OtherComponent,
     CameraComponent,
     LocationComponent,
+    CheckAppComponent,
 ];
---
+++ app/pages/check-app/check-app.component.ts
@@ -0,0 +1,44 @@
+import {Component} from "@angular/core";
+import * as dialog from 'ui/dialogs';
+
+import * as appAvailability from 'nativescript-appavailability';
+import * as app from "application";
+
+declare const android;
+
+@Component({
+    selector: "check-app",
+    templateUrl: 'pages/check-app/check-app.html',
+
+})
+export class CheckAppComponent {
+    checkStatus:string;
+    constructor() {
+        this.checkStatus='Did not check yet. Click button above';
+    }
+
+    check() {
+        let appToCheck = 'com.joelapenna.foursquared';
+        this.checkStatus = 'checking';
+
+        appAvailability.available(appToCheck).then((result:boolean) => {
+            if(result) {
+                console.log('yes');
+                this.checkStatus = 'app is there';
+            } else {
+                console.log('no');
+                this.checkStatus = 'app not installed';
+
+                const Intent = android.content.Intent;
+                const Uri = android.net.Uri;
+
+                let intent = new Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + appToCheck)
+                );
+                app.android.currentContext.startActivity(intent);
+
+            }
+        });
+    }
+}
